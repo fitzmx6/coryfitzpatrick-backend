50b63e6d7d7d7e7b900d0144b36ca222a26d2eb5
Cory Fitzpatrick <cfitzp15@its.jnj.com>
2025-09-05 12:27:04 -0400

    Cleanup unused import

--- src/main/java/com/api/coryfitzpatrick/controller/ContentController.java
+++ src/main/java/com/api/coryfitzpatrick/controller/ContentController.java
@@ -6,7 +6,6 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
